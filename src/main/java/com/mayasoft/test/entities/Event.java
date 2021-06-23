cf60bf2ed451be130319760288f750f04bf5c330
omar <omar.zuluaga@beamar.co>
2021-06-23 14:28:08 -0500

    event entity created

---
+++ src/main/java/com/mayasoft/test/entities/Event.java
@@ -0,0 +1,69 @@
+package com.mayasoft.test.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date start;
+    private Date end;
+    private String type;
+    private String description;
+
+    public Event () {}
+
+    public Event(Date start, Date end, String type, String description) {
+        this.start = start;
+        this.end = end;
+        this.type = type;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
