25532022146dc55f0a3932da5729086554c926bb
omar <omar.zuluaga@beamar.co>
2021-06-23 14:18:41 -0500

    instructor entity created

---
+++ src/main/java/com/mayasoft/test/entities/Instructor.java
@@ -0,0 +1,60 @@
+package com.mayasoft.test.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private Date birthday;
+
+    public Instructor () {}
+
+    public Instructor(String firstName, String lastName, Date birthday) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
